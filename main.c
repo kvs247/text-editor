b8d3636643dfc2e7b03c670a867c954c99db6d8c
Kyle <kylesch115@gmail.com>
2024-05-22 20:16:17 -0600

    enable/disable terminal raw mode

--- main.c
+++ main.c
@@ -1,8 +1,78 @@
+/*** includes ***/
+
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <termios.h>
+
+/*** data ***/
+
+struct termios original_termios;
+
+/*** terminal ***/
+
+void die(const char *s)
+{
+  perror(s);
+  exit(1);
+}
+
+void disableRawMode()
+{
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
+  {
+    die("tcsetattr");
+  }
+}
+
+void enableRawMode()
+{
+  if (tcgetattr(STDIN_FILENO, &original_termios) == -1)
+  {
+    die("tcgetattr");
+  }
+  atexit(disableRawMode);
+
+  struct termios raw = original_termios;
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+  {
+    die("tcsetattr");
+  }
+}
+
+/*** init ***/
 
 int main()
 {
-    printf("Text Editor\n");
+  enableRawMode();
 
-    return 0;
+  while (1)
+  {
+    char c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+    {
+      die("read");
+    }
+    if (iscntrl(c))
+    {
+      printf("%d\r\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q')
+      break;
+  }
+
+  return 0;
 }
