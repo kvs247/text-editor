b474bb09b01e40255b9337bc095d96c87bf2a5ac
Kyle <kylesch115@gmail.com>
2024-05-23 18:37:31 -0600

    add comments for step 1

--- main.c
+++ main.c
@@ -13,12 +13,14 @@ struct termios original_termios;
 
 /*** terminal ***/
 
+// error handling, use with library calls
 void die(const char *s)
 {
   perror(s);
   exit(1);
 }
 
+// set terminal settings to original at program start
 void disableRawMode()
 {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios) == -1)
@@ -27,8 +29,10 @@ void disableRawMode()
   }
 }
 
+// edit terminal settings
 void enableRawMode()
 {
+  // get terminal settings at program start and write to "original_termios"
   if (tcgetattr(STDIN_FILENO, &original_termios) == -1)
   {
     die("tcgetattr");
@@ -36,10 +40,14 @@ void enableRawMode()
   atexit(disableRawMode);
 
   struct termios raw = original_termios;
+  
+  // update various terminal flags
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= ~(CS8);
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+  // set "read" to timeout after 100ms & return after 1 byte recieved
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
@@ -58,10 +66,13 @@ int main()
   while (1)
   {
     char c = '\0';
+
+    // read 1 byte from stdin into "c" until all bytes have been read
     if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
     {
       die("read");
     }
+    // display keypresses
     if (iscntrl(c))
     {
       printf("%d\r\n", c);
@@ -70,6 +81,7 @@ int main()
     {
       printf("%d ('%c')\r\n", c, c);
     }
+    // quit program
     if (c == 'q')
       break;
   }
