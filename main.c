73a877a631795f38f6e8e893301b2fa880a18c2b
Kyle <kylesch115@gmail.com>
2024-06-12 18:39:28 -0600

    save function

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -65,6 +66,10 @@ struct editorConfig
 };
 
 struct editorConfig E;
+
+/*** prototypes ***/
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 /*** terminal ***/
 
@@ -362,6 +367,29 @@ void editorInsertChar(int c)
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen)
+{
+  int totlen = 0;
+  int j;
+  for (j = 0; j < E.numrows; j++)
+  {
+    totlen += E.row[j].size + 1;
+  }
+  *buflen = totlen;
+
+  char *buf = malloc(totlen);
+  char *p = buf;
+  for (j = 0; j < E.numrows; j++)
+  {
+    memcpy(p, E.row[j].chars, E.row[j].size);
+    p += E.row[j].size;
+    *p = '\n';
+    p++;
+  }
+
+  return buf;
+}
+
 void editorOpen(char *filename)
 {
   free(E.filename);
@@ -386,6 +414,38 @@ void editorOpen(char *filename)
   }
   free(line);
   fclose(fp);
+}
+
+void editorSave()
+{
+  if (E.filename == NULL)
+  {
+    return;
+  }
+
+  int len;
+  char *buf = editorRowsToString(&len);
+
+  // 0644 standard permissions for opening file
+  // gives read/write to file owner and read to all else
+  int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+  if (fd != -1)
+  {
+    if (ftruncate(fd, len) != -1)
+    {
+      if (write(fd, buf, len) == len)
+      {
+        close(fd);
+        free(buf);
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
+
+  free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 /*** append buffer ***/
@@ -644,6 +704,10 @@ void editorProcessKeypress()
     exit(0);
     break;
 
+  case CTRL_KEY('s'):
+    editorSave();
+    break;
+
   case HOME_KEY:
     E.cx = 0;
     break;
@@ -733,7 +797,7 @@ int main(int argc, char *argv[])
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-Q = quit");
+  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
   while (1)
   {
